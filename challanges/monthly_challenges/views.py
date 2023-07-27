035da154301e61e33786ee69d6d39d984e8918bf
Alpesh Valaki <alpeshvalaki@gmail.com>
2023-07-27 11:22:01 +0530

    [IMP] added dynamic url based on given datatype

--- challanges/monthly_challenges/views.py
+++ challanges/monthly_challenges/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
-monthly_challenges = {}
-
+monthly_challenge = {'january': "Eat no meat for the entire month!",
+                    'february': "Walk for at least 20 minutes every day!",
+                    'march': "Learn Django for at least 20 minutes every day!",
+                    'april': "Eat no meat for the entire month!",
+                    'may': "Walk for at least 20 minutes every day!",
+                    'june': "Learn Django for at least 20 minutes every day!",
+                    'july': "Eat no meat for the entire month!",
+                    'august': "Walk for at least 20 minutes every day!",
+                    'september': "Learn Django for at least 20 minutes every day!",
+                    'october': "Eat no meat for the entire month!",
+                    'november': "Walk for at least 20 minutes every day!",
+                    'december': "read for at least 20 minutes every day!"}
 
 def january(request):
     return HttpResponse("Eat no meat for the entire month!")
@@ -12,14 +22,17 @@ def january(request):
 def february(request):
     return HttpResponse("Walk for at least 20 minutes every day!")
 
+def monthly_challenges_by_number(request, month=1):
+    if month > 12:
+        return HttpResponseNotFound("This month is not supported!")
+    months = list(monthly_challenge.keys())
+    months = months[month-1]
+    return HttpResponseRedirect("/month/" + months + "/")
+
+
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 minutes every day!"
+    challenge_text = monthly_challenge.get(month)
+    if challenge_text:
+        return HttpResponse(challenge_text)
     else:
-        return HttpResponse("This month is not supported!")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("This month is not supported!")
